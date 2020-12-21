2ae9fdad79b8368768d209fa7b733f100d3b9883
kalbyimanie <aby.imanie@gmail>
2020-12-21 18:07:40 +0700

    added and modified files

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ANSIBLE_CONFIG=~/playground/config/ansible.cfg ANSIBLE_DEBUG=$1 ansible-playbook ~/playground/playbooks/slave1-playbook.yml -i ~/playground/hosts/inventory
+ANSIBLE_CONFIG=~/playground/config/ansible.cfg ANSIBLE_DEBUG=$1 ansible-playbook ~/playground/playbooks/setup-nginx-playbook.yml -i ~/playground/hosts/inventory --vault-password-file=../.vault_password
